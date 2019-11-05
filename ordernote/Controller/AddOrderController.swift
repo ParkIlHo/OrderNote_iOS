f65c1ac7de8e9df627ee8c0fbaca4d0c16b0f2f3
ian <ian.park@koreapolyschool.com>
2019-11-05 18:57:38 +0900

    ian.park : add AddOrderController

---
+++ ordernote/Controller/AddOrderController.swift
@@ -0,0 +1,15 @@
+//
+//  AddOrderController.swift
+//  ordernote
+//
+//  Created by 박일호 on 05/11/2019.
+//  Copyright © 2019 박일호. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class AddOrderController: UIViewController {
+    
+}
--- ordernote/Controller/OrderController.swift
+++ ordernote/Controller/OrderController.swift
@@ -10,8 +10,30 @@ import Foundation
 import UIKit
 
 class OrderController: UIViewController {
+    
+    // MARK: override
     override func viewDidLoad() {
         self.navigationController?.navigationBar.tintColor = .black
         self.navigationController?.navigationBar.topItem?.title = "title_order".localized
+        
+        var rightBtnImage = UIImage(named: "add_order")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBtnImage, style: .done, target: self, action: #selector(addOrder))
+    }
+    
+    // MARK: Add Order View
+    @objc func addOrder() {
+        print("click add Order!!!!!")
+        let addOrder = self.storyboard?.instantiateViewController(withIdentifier: "AddOrder") as! AddOrderController
+        //            changePassword?.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
+        //            changePassword?.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
+        //            changePassword?.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+                    
+        addOrder.providesPresentationContextTransitionStyle = true
+        addOrder.definesPresentationContext = true
+        addOrder.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
+        
+//        addOrder.delegate = self
+                    
+        self.present(addOrder, animated: true, completion: nil)
     }
 }
